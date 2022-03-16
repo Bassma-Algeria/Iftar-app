7f2f35d74ed1905f428a9eb2a181c3c5f6808afd
Yasser-Belatreche <yasser.belatreche0@gmail.com>
2022-03-16 18:06:42 +0100

    map init

--- React-Native/src/screens/HomeStack/screens/Map/Map.style.tsx
+++ React-Native/src/screens/HomeStack/screens/Map/Map.style.tsx
@@ -1,5 +1,16 @@
 import {StyleSheet} from 'react-native';
+import {SPACING} from '../../../../theme/Spacings';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export {styles};
--- React-Native/src/screens/HomeStack/screens/Map/Map.tsx
+++ React-Native/src/screens/HomeStack/screens/Map/Map.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View} from 'react-native';
+import MapView from 'react-native-maps';
 
 import {styles} from './Map.style';
 
@@ -9,9 +10,11 @@ import {Header} from '../../../../components/Header/Header';
 
 interface Props extends HomeStackScreenProps<'Map'> {}
 
-const Map: React.FC<Props> = ({navigation}) => {
+const Map: React.FC<Props> = () => {
   return (
-    <View>
+    <View style={styles.container}>
+      <MapView showsUserLocation style={styles.map} />
+
       <Header>Map</Header>
     </View>
   );
